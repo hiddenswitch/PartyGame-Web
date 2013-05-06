50ec89f6b6d1b89dd99147755179662e35ab4a58
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-05-05 22:30:01 -0500

    Adding default settings, moving keys into settings file.

--- server/libs/defaults.js
+++ server/libs/defaults.js
@@ -1,4 +1,8 @@
-{
+/**
+ * @author Benjamin Berman
+ * © 2012 All Rights Reserved
+ **/
+if (_.isEmpty(Meteor.settings)) Meteor.settings = {
     "useBots":true,
     "facebook":{
         "appSecret":"2534aa63fd9dbe165d1a284842f46d2a",
@@ -8,4 +12,6 @@
         "clientId":"253853968266.apps.googleusercontent.com",
         "clientSecret":"l4GGLNFGBiGz8bebefmSbkTz"
     }
-}
+};
+
+console.log("Meteor.settings: " + JSON.stringify(Meteor.settings));
