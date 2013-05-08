78331ec190ec4fed0c54667d8451b7f9faa25c54
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-05-07 21:47:32 -0700

    Improving maintenance functions.

--- server/maintenance.js
+++ server/maintenance.js
@@ -5,13 +5,25 @@
 Meteor.startup(function() {
     // Close games that haven't seen any activity for a while, delete games that have been closed for a while
     Meteor.setInterval(function () {
-        Games.update({open:true,$or:[{modified:{$lt:new Date().getTime() - K_HEARTBEAT*20}},{questionCardsCount:0},{answerCardsCount:0}]},{$set:{open:false}},{multi:true});
-        Meteor.call("clean");
+        Games.update({open:true,$or:[{modified:{$lt:new Date().getTime() - K_HEARTBEAT*20}},{questionCardsCount:{$lt:1}},{answerCardsCount:{$lt:1}}]},{$set:{open:false}},{multi:true},function(e) {
+            if (e) {
+                console.log(e);
+            }
+        });
+
+        Meteor.call("clean",function(e,r){
+            if (r) {
+                console.log(r);
+            }
+            if (e) {
+                console.log(e);
+            }
+        });
     },40*K_HEARTBEAT);
 
     // Update player connected status. Bots are always connected
     Meteor.setInterval(function () {
-        var disconnectedUsers = Meteor.users.find({'profile.bot':false,'profile.heartbeat':{$lt:new Date().getTime() - K_HEARTBEAT*2}}).fetch();
+        var disconnectedUsers = Meteor.users.find({'profile.bot':{$nin:[true]},'profile.heartbeat':{$lt:new Date().getTime() - K_HEARTBEAT*2}}).fetch();
 
         // Set the connected attribute of the Players collection documents to false for disconnected users
         Players.update({userId:{$in:_.pluck(disconnectedUsers,'_id')},connected:true},{$set:{connected:false}},{multi:true});
@@ -29,15 +41,25 @@ Meteor.startup(function() {
 
 Meteor.methods({
     clean:function() {
-        if (this.userId) {
-            throw new Meteor.Error(503,"You must be an administrator to call this function.");
-        }
-
+        console.log("Cleaning...");
+        var o = {};
         var closedGames = _.pluck(Games.find({open:false},{fields:{_id:1}}).fetch(),"_id");
+        o.closedGames = closedGames.length;
 
         Hands.remove({gameId:{$in:closedGames}});
         Submissions.remove({gameId:{$in:closedGames}});
         Votes.remove({gameId:{$in:closedGames}});
+
+        o.closedPlayers = Players.find({}).count();
+
         Players.remove({$or:[{gameId:{$in:closedGames}},{open:false}]});
+
+        o.closedPlayers -= Players.find({}).count();
+
+        Games.remove({$or:[{_id:{$in:closedGames}},{open:false}]});
+
+        console.log(JSON.stringify(o));
+
+        return o;
     }
 });
