0ec6a681ba61f1e1f8d8669e2f941f0a619295b0
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-05-26 20:08:43 -0700

    Hotfixing local distance calculations

--- params.js
+++ params.js
@@ -4,6 +4,6 @@
  **/
 K_DEFAULT_HAND_SIZE = 8; // default hand size
 K_HEARTBEAT = 8 * 1000; // default heartbeat length
-K_LOCAL_DISTANCE = 0.0003; // distance in lat-lon units, approximately 150 ft (?)
+K_LOCAL_DISTANCE = 0.003; // distance in lat-lon units, approximately 150 ft (?)
 K_PREFERRED_GAME_SIZE = 7; // the size of a game matchmaking prefers to make
 K_BLANKS = "████";
