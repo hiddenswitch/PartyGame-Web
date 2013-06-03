7e7ebba93f18dea99be871e74275064f29e5b066
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-06-03 12:27:21 -0700

    Create new user fields

--- server/newUserFlow.js
+++ server/newUserFlow.js
@@ -2,12 +2,26 @@
  * @author Benjamin Berman
  * © 2012 All Rights Reserved
  **/
+
+var K_INITIAL_COINS = 100;
+
 // Configure user profiles
 Accounts.onCreateUser(function(options, user) {
-    if (options.profile)
+    if (options.profile) {
         user.profile = options.profile;
-    else
+    } else {
         user.profile = {};
-    user.heartbeat = new Date().getTime();
+    }
+
+    var now = new Date().getTime();
+
+    user.heartbeat = now;
+    user.lastAction = now;
+    user.questionIds = [];
+    user.score = 0;
+    user.coins = K_INITIAL_COINS;
+    user.inventory = {decks:['Cards Against Humanity','Starter']};
+    user.matchingValue = 0;
+
     return user;
 });
