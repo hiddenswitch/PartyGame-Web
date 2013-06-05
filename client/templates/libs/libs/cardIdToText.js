0f028ef055a35c2dafd5e1d2b381860507b68622
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-06-04 19:41:40 -0700

    improving card id to text

--- client/templates/libs/libs/cardIdToText.js
+++ client/templates/libs/libs/cardIdToText.js
@@ -3,5 +3,5 @@
  * © 2012 All Rights Reserved
  **/
 cardIdToText = function (cardId) {
-    return _.extend({text: null}, Cards.findOne({_id: cardId})).text;
+    return _.extend({text: null}, Cards.findOne({_id: cardId}, {fields: {text: 1}, limit: 1})).text;
 };
