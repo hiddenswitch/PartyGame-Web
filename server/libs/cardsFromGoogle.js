1c6b7383f27ac7b6ab8d777e5b59b3e681501c31
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-06-04 20:15:55 -0700

    Updating card import

--- server/libs/cardsFromGoogle.js
+++ server/libs/cardsFromGoogle.js
@@ -5,43 +5,46 @@
 var URL_TO_CARDS_TSV = "https://dl.dropboxusercontent.com/u/2891540/cards.tsv";
 
 Meteor.methods({
-    updateCardsFromGoogle:function() {
+    updateCardsFromGoogle: function () {
         (function (e, r) {
             if (r) {
-                if (Cards.find().count() === 0) {
-                    var cardsCounted = 0;
-                    var cards = _.compact(_.map(r.content.split('\n').splice(1), function (o) {
-                        o = o.split('\t');
+                var cardsCounted = 0;
+                var cards = _.compact(_.map(r.content.split('\n').splice(1), function (o) {
+                    o = o.split('\t');
 
-                        if (o && o.length > 4) {
-                            return {deck: o[1],
-                                category: o[2],
-                                type: o[3] == "Answer" ? CARD_TYPE_ANSWER : CARD_TYPE_QUESTION,
-                                text: o[4]};
-                        } else {
-                            return null;
-                        }
-                    }));
+                    if (o && o.length > 4 && o[0] !== 'Ignore') {
+                        return {deck: o[1],
+                            category: o[2],
+                            type: o[3] == "Answer" ? CARD_TYPE_ANSWER : CARD_TYPE_QUESTION,
+                            text: o[4],
+                            random: Math.random()
+                        };
+                    } else {
+                        return null;
+                    }
+                }));
 
-                    _.each(cards,function(c) {
+                _.each(cards, function (c) {
+                    var formattedText = c.text.replace(/_+/, K_BLANKS);
+                    if (Cards.find({text: formattedText}).count() === 0) {
                         var deckId = null;
                         if (Decks.find({title: c.deck}).count() === 0) {
-                            deckId = Decks.insert({title: c.deck, description:"From the Redacted Team, a " + c.category.toLowerCase() + " deck."});
+                            deckId = Decks.insert({title: c.deck, description: "From the Redacted Team, a " + c.category.toLowerCase() + " deck."});
                         } else {
-                            deckId = Decks.findOne({title: c.deck},{fields:{_id:1}})._id;
+                            deckId = Decks.findOne({title: c.deck}, {fields: {_id: 1}})._id;
                         }
 
-                        c.text = c.text.replace(/_+/,K_BLANKS);
+                        c.text = formattedText;
 
-                        Cards.insert(_.extend(c,{deckId:deckId}));
+                        Cards.insert(_.extend(c, {deckId: deckId}));
                         cardsCounted++;
-                    });
-                    console.log(JSON.stringify({cardsCounted:0}));
-                }
+                    }
+                });
+                console.log(JSON.stringify({cardsCounted: 0}));
             }
             if (e) {
                 console.log(e);
             }
-        })(null,Meteor.http.get(URL_TO_CARDS_TSV));
+        })(null, Meteor.http.get(URL_TO_CARDS_TSV));
     }
 });
