3b753018cb8767d3de2b20c9ac75eb99d97a4715
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-06-12 13:03:49 -0700

    cleaning up inset lists

--- client/templates/libs/browse.js
+++ client/templates/libs/browse.js
@@ -11,11 +11,7 @@ Template.browse.created = defaultCreated;
 Template.browse.preserve(defaultPreserve);
 
 Template.browse.games = function() {
-    if (Session.equals("currentPage","browse")) {
-        return Games.find({open:true},{limit:10,sort:{players:-1},fields:{_id:1,players:1,title:1,open:1}});
-    } else {
-        return null;
-    }
+    return Games.find({open:true},{limit:10,sort:{players:-1},fields:{_id:1,players:1,title:1,open:1}});
 };
 
 Template.browse.events = {
