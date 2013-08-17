4482f9d2a71d58b538cfeca6b0e8bd3ab4b823eb
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-08-17 15:19:44 -0700

    Give users booster packs.

--- server/inventory.js
+++ server/inventory.js
@@ -2,8 +2,38 @@
  * @author Benjamin Berman
  * © 2012 All Rights Reserved
  **/
+
+K_SERVER = "server";
+K_BOOSTER_PACK_SIZE = 15;
+
 InventoryManager = {
-    openBoosterPacks: function(count) {
+    openBoosterPacks: function (count) {
 
     }
-}
+};
+
+Meteor.methods({
+    trade: function (commodityId1, commodityQuantity1, partyId1, commodityId2, commodityQuantity2, partyId2, _userId) {
+
+    },
+
+    creditBoosterPack: function (_userId) {
+        if (!this.userId && !_userId) {
+            throw new Meteor.Error(500, "When server calls alchemyCombine, you must impersonate a user.");
+        } else if (_userId == null && this.userId) {
+            _userId = this.userId;
+        } else {
+            throw new Meteor.Error(403, "Permission denied.");
+        }
+
+        var user = Meteor.users.findOne({_id: _userId});
+
+        if (user == null) {
+            throw new Meteor.Error(404, "User {0} not found.".format(_userId));
+        }
+
+        var cardIds = CardManager.getCardIdMix(K_BOOSTER_PACK_SIZE);
+
+        Meteor.users.update({_id: _userId}, {$push: {"inventory.cards": cardIds}});
+    }
+});
