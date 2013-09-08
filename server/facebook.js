e1ea22635eedc468810265aae66262a9581a7a63
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-09-08 14:51:50 +0200

    Sending questions and portrait grid.

--- server/facebook.js
+++ server/facebook.js
@@ -2,21 +2,42 @@
  * @author Benjamin Berman
  * © 2012 All Rights Reserved
  **/
-var FacebookManager = {
-    facebookNpm: Meteor.require('facebook-node-sdk')
+FacebookManager = {
+    facebookNpm: Meteor.require('facebook-node-sdk'),
+    fb: function (accessToken) {
+        if (accessToken == null) return null;
+        return new FacebookManager.facebookNpm({
+            appID: Meteor.settings.facebook.appId,
+            secret: Meteor.settings.facebook.appSecret
+        }).setAccessToken(accessToken);
+    },
+    accessToken: function (userId) {
+        if (userId == null) return null;
+        var u = Meteor.users.findOne({_id: userId});
+        if (u && u.services && u.services.facebook) {
+            return u.services.facebook.accessToken;
+        } else {
+            return null;
+        }
+    }
 };
 
-FacebookManager.facebook = new FacebookManager.facebookNpm({
-    appID: Meteor.settings.facebook.appId,
-    secret: Meteor.settings.facebook.appSecret
-});
+Meteor.publish('fbFriends', function () {
+    var self = this;
+    var fb = FacebookManager.fb(FacebookManager.accessToken(this.userId));
 
-Meteor.methods({
-    'facebookQuery':function() {
-        var query = Meteor.sync(function(done) {
-            FacebookManager.facebook.api('/benjamin.berman.16', done);
-        });
-
-        return query;
+    if (fb == null) {
+        self.ready();
+        return;
     }
+
+    var query = Meteor.sync(function (done) {
+        fb.api('/me/friends?fields=name,id,picture', done);
+    });
+
+    _.each(query.result.data, function (friend) {
+        self.added('fbFriends', friend.id, _.omit(friend, 'id'));
+    });
+
+    self.ready();
 });
---
+++ server/lib/constants/avatars.js
@@ -0,0 +1,5 @@
+/**
+ * @author Benjamin Berman
+ * © 2012 All Rights Reserved
+ **/
+DefaultAvatars = ["Ben.jpg", "Sergio.jpg", "unknown-user-0@2x.png", "unknown-user-10@2x.png", "unknown-user-1@2x.png", "unknown-user-2@2x.png", "unknown-user-3@2x.png", "unknown-user-4@2x.png", "unknown-user-5@2x.png", "unknown-user-6@2x.png", "unknown-user-7@2x.png", "unknown-user-8@2x.png", "unknown-user-9@2x.png"];
