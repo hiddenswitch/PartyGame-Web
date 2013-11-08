469b838d6cca0fed24301846655cfd7006124481
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-11-07 17:14:12 -0800

    Refactoring publishes and index creation

---
+++ server/indices.js
@@ -0,0 +1,21 @@
+/**
+ * @author Benjamin Berman
+ * © 2012 All Rights Reserved
+ **/
+Meteor.startup(function () {
+    // enable the geospatial index on games and users
+    Games._ensureIndex({location: "2d"});
+    Games._ensureIndex({open: 1, modified: -1, userIds: 1});
+    Votes._ensureIndex({gameId: 1});
+    Hands._ensureIndex({gameId: 1});
+    Hands._ensureIndex({userId: 1});
+    Cards._ensureIndex({deckId: 1});
+    Decks._ensureIndex({title: 1});
+    Cards._ensureIndex({type: 1});
+    Players._ensureIndex({userId: 1});
+    Players._ensureIndex({gameId: 1, userId: 1, connected: 1});
+    Submissions._ensureIndex({gameId: 1});
+    Meteor.users._ensureIndex({heartbeat: -1});
+    Meteor.users._ensureIndex({location: "2d"});
+    Inventories._ensureIndex({userId: 1, itemType: 1});
+});
