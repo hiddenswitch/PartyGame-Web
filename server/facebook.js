cacf85e42dc6e477eb4fbbbe301fbbae54fd43e6
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-11-07 21:00:29 -0800

    Improve friend invite flow.

--- server/facebook.js
+++ server/facebook.js
@@ -90,14 +90,27 @@ Meteor.publish('fbFriends', function () {
         return;
     }
 
-    var fqlQuery = Meteor.sync(function (done) {
-        var mutualFriendQuery = encodeURIComponent('SELECT uid, name, pic_square, mutual_friend_count FROM user WHERE uid IN (SELECT uid1 FROM friend WHERE uid2 = me()) ORDER BY name ASC LIMIT 200').replace(/%20/g, '+');
-        fb.api('/fql?q=' + mutualFriendQuery, done);
-    });
+    if (Meteor.settings.useFql) {
+        var fqlQuery = Meteor.sync(function (done) {
+            var ordinaryFriendQuery = encodeURIComponent('SELECT uid1, name, pic_square FROM friend WHERE uid2=me()');
+            var mutualFriendQuery = encodeURIComponent('SELECT uid, name, pic_square, mutual_friend_count FROM user WHERE uid IN (SELECT uid1 FROM friend WHERE uid2 = me()) ORDER BY name ASC LIMIT 200').replace(/%20/g, '+');
+            fb.api('/fql?q=' + mutualFriendQuery, done);
+        });
 
-    _.each(fqlQuery.result.data, function (friend) {
-        self.added('fbFriends', friend.uid, _.omit(friend, 'uid'));
-    });
+        _.each(fqlQuery.result.data, function (friend) {
+            self.added('fbFriends', friend.uid, _.omit(friend, 'uid'));
+        });
+
+    } else {
+        var pictureQuery = Meteor.sync(function (done) {
+            var query = "/me/friends?fields=name,picture";
+            fb.api(query, done);
+        });
+
+        _.each(pictureQuery.result.data, function (friend) {
+            self.added('fbFriends', friend.id, {name: friend.name, pic_square: friend.picture.data.url});
+        });
+    }
 
     self.ready();
 });
