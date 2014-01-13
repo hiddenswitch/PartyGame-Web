ad6ba1bf858171e66bca8a2654e79aa6c693f0f6
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-01-12 18:12:01 -0800

    Collection routes

--- server/routes.js
+++ server/routes.js
@@ -7,4 +7,47 @@ Meteor.startup(function () {
     HTTP.publish(Cards, function () {
         return Cards.find({});
     });
+
+    HTTP.publish(Games, function () {
+        return Games.find({open: true}, {fields: {password: 0, questionCards: 0, answerCards: 0}});
+    });
+
+    HTTP.publish(Questions, function () {
+        return Questions.find({$or: [
+            {judgeId: this.userId},
+            {userIds: this.userId}
+        ]});
+    });
+
+    HTTP.publish(Histories, function () {
+        return Histories.find({userId: this.userId});
+    });
+
+    HTTP.publish(Answers, function () {
+        return Answers.find({userIds: this.userId});
+    });
+
+    HTTP.publish(Hands, function () {
+        return Hands.find({userId: this.userId});
+    });
+
+    HTTP.publish(Players, function () {
+        return Players.find({gameId: this.query.gameId});
+    });
+
+    HTTP.publish(Submissions, function (gameId) {
+        return Submissions.find({gameId: this.query.gameId}, {fields: {_id: 1, gameId: 1, answerId: 1, round: 1}});
+    });
+
+    Meteor.publish(Votes, function () {
+        return Votes.find({gameId: this.query.gameId});
+    });
+
+    Meteor.publish(Avatars, function () {
+        return Avatars.find({userId: this.userId});
+    });
+
+    Meteor.publish(Inventories, function () {
+        return Inventories.find({userId: this.userId});
+    });
 });
