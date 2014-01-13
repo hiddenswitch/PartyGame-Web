e4990d1f42ac5f437ba3a7b4fc98a6b7c0ebb3af
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-01-12 22:44:08 -0800

    Don't delete old games so aggressively.

--- server/maintenance.js
+++ server/maintenance.js
@@ -2,16 +2,20 @@
  * @author Benjamin Berman
  * © 2012 All Rights Reserved
  **/
-Meteor.startup(function() {
+Meteor.startup(function () {
     // Close games that haven't seen any activity for a while, delete games that have been closed for a while
     Meteor.setInterval(function () {
-        Games.update({open:true,$or:[{modified:{$lt:new Date().getTime() - K_HEARTBEAT*100}},{questionCardsCount:{$lt:1}},{answerCardsCount:{$lt:1}}]},{$set:{open:false}},{multi:true},function(e) {
+        Games.update({open: true, $or: [
+            {modified: {$lt: new Date().getTime() - K_HEARTBEAT * 10000}},
+            {questionCardsCount: {$lt: 1}},
+            {answerCardsCount: {$lt: 1}}
+        ]}, {$set: {open: false}}, {multi: true}, function (e) {
             if (e) {
                 console.log(e);
             }
         });
 
-        Meteor.call("clean",function(e,r){
+        Meteor.call("clean", function (e, r) {
             if (r) {
                 console.log(r);
             }
@@ -19,44 +23,50 @@ Meteor.startup(function() {
                 console.log(e);
             }
         });
-    },50*K_HEARTBEAT);
+    }, 50 * K_HEARTBEAT);
 
     // Update player connected status. Bots are always connected
     Meteor.setInterval(function () {
-        var disconnectedUsers = Meteor.users.find({bot:{$ne:true},heartbeat:{$lt:new Date().getTime() - K_HEARTBEAT*2}}).fetch();
+        var disconnectedUsers = Meteor.users.find({bot: {$ne: true}, heartbeat: {$lt: new Date().getTime() - K_HEARTBEAT * 2}}).fetch();
 
         // Set the connected attribute of the Players collection documents to false for disconnected users
-        Players.update({userId:{$in:_.pluck(disconnectedUsers,'_id')},connected:true},{$set:{connected:false}},{multi:true});
+        Players.update({userId: {$in: _.pluck(disconnectedUsers, '_id')}, connected: true}, {$set: {connected: false}}, {multi: true});
 
         // Update the judges
-        _.each(Games.find({open:true},{fields:{_id:1,judgeId:1}}).fetch(),function(g){
-            var gameCurrentJudge = Meteor.call("currentJudge",g._id);
+        _.each(Games.find({open: true}, {fields: {_id: 1, judgeId: 1}}).fetch(), function (g) {
+            var gameCurrentJudge = Meteor.call("currentJudge", g._id);
             if (g.judgeId !== gameCurrentJudge) {
-                Games.update({_id:g._id},{$set:{judgeId:gameCurrentJudge}});
+                Games.update({_id: g._id}, {$set: {judgeId: gameCurrentJudge}});
             }
         });
 
-    },2*K_HEARTBEAT);
+    }, 2 * K_HEARTBEAT);
 });
 
 Meteor.methods({
-    clean:function() {
+    clean: function () {
         console.log("Cleaning...");
         var o = {};
-        var closedGames = _.pluck(Games.find({open:false},{fields:{_id:1}}).fetch(),"_id");
+        var closedGames = _.pluck(Games.find({open: false}, {fields: {_id: 1}}).fetch(), "_id");
         o.closedGames = closedGames.length;
 
-        Hands.remove({gameId:{$in:closedGames}});
-        Submissions.remove({gameId:{$in:closedGames}});
-        Votes.remove({gameId:{$in:closedGames}});
+        Hands.remove({gameId: {$in: closedGames}});
+        Submissions.remove({gameId: {$in: closedGames}});
+        Votes.remove({gameId: {$in: closedGames}});
 
         o.closedPlayers = Players.find({}).count();
 
-        Players.remove({$or:[{gameId:{$in:closedGames}},{open:false}]});
+        Players.remove({$or: [
+            {gameId: {$in: closedGames}},
+            {open: false}
+        ]});
 
         o.closedPlayers -= Players.find({}).count();
 
-        Games.remove({$or:[{_id:{$in:closedGames}},{open:false}]});
+        Games.remove({$or: [
+            {_id: {$in: closedGames}},
+            {open: false}
+        ]});
 
         console.log(JSON.stringify(o));
 
