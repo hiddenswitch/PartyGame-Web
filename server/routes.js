ed7bd3d27ef5f1d2f1d93e49837b3c17a5e7b039
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-01-13 10:41:10 -0800

    Temporary routes work

--- server/routes.js
+++ server/routes.js
@@ -52,6 +52,70 @@ Meteor.startup(function () {
         return Inventories.find({userId: this.userId});
     });
 
-    // Methods
-
+//    var methods = [
+//        ['submitAnswerCard', 'gameId', 'answerId', 'playerId'],
+//        ['pickWinner', 'gameId', 'submissionId'],
+//        ['finishRound', 'gameId'],
+//        ['kickPlayer', 'gameId', 'kickId'],
+//        ['quitGame', 'gameId'],
+//        ['currentJudge', 'gameId'],
+//        ['closeGame', 'gameId'],
+//        ['heartbeat', 'currentLocation'],
+//        ['drawHands', 'gameId', 'handSize'],
+//        ['findGameWithUser', 'userId'],
+//        ['joinGameWithTitle', 'title'],
+//        ['joinOrCreateGameWithTitle', 'title'],
+//        ['joinGame', 'gameId'],
+//        ['findGameWithFewPlayers', 'gameSize'],
+//        ['findLocalGame', 'location'],
+//        ['findAnyGame'],
+//        ['createEmptyGame', 'title', 'password', 'location'],
+//        ['sendQuestion', 'questionCardId', 'toUserIds'],
+//        ['writeAnswer', 'historyId', 'answerCardId'],
+//        ['getQuestionForUser'],
+//        ['pickAnswer', 'answerId'],
+//        ['registerForPush', 'runtimePlatform', 'deviceToken'],
+//        ['inviteFriendsToGame', 'fbIds', 'message'],
+//        ['facebookLoginWithAccessToken', 'id', 'email', 'name', 'accessToken'],
+//        /*
+//         * options: {profile, email, password, username}
+//         * */
+//        ['createUser', 'options'],
+//        /*
+//         * options: {resume: token}
+//         * or
+//         * options: {password, user: {username}}
+//         * */
+//        ['login', 'options']
+//    ];
+//
+//    // Define stub impersonation methods
+//    var secretKey = Meteor.uuid() + Meteor.uuid();
+//    Meteor.methods(_.object(_.map(methods,function(m) {
+//        return [secretKey + m[0],function(args,userId) {
+//            this.setUserId(userId);
+//            console.log(userId);
+//            console.log("executed");
+//            return Meteor.call.apply(this,[m[0]].concat(args));
+//        }];
+//    })));
+//
+//    // Methods
+//    HTTP.methods(_.object(_.map(methods, function (m) {
+//        return ['/api/' + m[0], function (data) {
+////            try {
+//                console.log(this.userId);
+//                console.log(JSON.stringify(data));
+//                console.log(secretKey+m[0]);
+//                var r = Meteor.call(secretKey+m[0],data,this.userId);
+//                console.log(JSON.stringify(r));
+//                return JSON.stringify({result: r,
+//                    error: null});
+////            } catch (e) {
+////                console.log(JSON.stringify(e));
+////                return JSON.stringify({result: null,
+////                    error: e});
+////            }
+//        }];
+//    })));
 });
