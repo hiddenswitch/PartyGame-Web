c4ab91d3d2d0f53b4459f297b2201f1b5205642f
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-03-20 11:15:23 -0700

    Remove experience junk

--- tests/Experience.ad/AdUnit/js/AnswerQuestionPage-view2.js
+++
@@ -1,6 +0,0 @@
-iAP.EventDelegate.registerViewEventDelegate('view2', 'AnswerQuestionPage', function() {
-
-this.onViewActivate = function (event) {
-	this.viewController.startActionListWithSourceView('(null)', this);
-}
-});
