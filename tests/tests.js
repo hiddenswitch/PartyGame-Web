96d93f92fe64d0e2a0d89335a7fd9d7026f71004
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-03-20 11:17:28 -0700

    Removing junk tests

--- tests/tests.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author Benjamin Berman
- * © 2012 All Rights Reserved
- **/
-
-// In browser to send questions to friends
-Meteor.call("sendQuestion",Cards.findOne({type:CARD_TYPE_QUESTION})._id, _.map(_.pluck(Friends.find().fetch(),'_id'),function(u) {return {"services.facebook.id":u};}));
