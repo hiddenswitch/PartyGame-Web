d504b82a2c75d4399728c718626a24f641cbdc27
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2014-11-09 20:29:07 -0800

    Migration for cards

--- server/migrations.js
+++ server/migrations.js
@@ -6,8 +6,20 @@
 Migrations.add({
     name: 'Load first version of cards',
     version: 1,
-    up: function() {
+    up: function () {
         // Retrieve a specific version of the cards
-
+        var cardSource = HTTP.get('https://raw.githubusercontent.com/hiddenswitch/PartyGame-Web/2f85e6abb255aa0f11ebd9f022f3430494f52f69/cards.js').content;
+        // Load cards
+        eval(cardSource);
+        // Clear all cards
+        Cards.remove({});
+        // Insert new cards
+        _.each(CardsSource, function (card) {
+            Cards.insert(card);
+        });
     }
+});
+
+Meteor.startup(function () {
+    Migrations.migrateTo(1);
 });
