14155db996fbcef9192d470e2b85a377e123f9fb
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-06-24 01:32:05 -0700

    Disable node-xmpp for now since browserify seems to seriously screw up modulus

--- packages/partygame-npm/package.js
+++ packages/partygame-npm/package.js
@@ -17,12 +17,9 @@ Package.onTest(function (api) {
 });
 
 
-if (process.env
-    && process.env.OFFLINE != 'true') {
-    Npm.depends({
-        "facebook-node-sdk": "0.2.0",
-        "node-xmpp": "0.12.2",
-        "apn": "1.4.3",
-        "markdown": "0.5.0"
-    });
-}
+Npm.depends({
+    "facebook-node-sdk": "0.2.0",
+    //"node-xmpp": "0.12.2",
+    "apn": "1.4.3",
+    "markdown": "0.5.0"
+});
